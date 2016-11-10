b0026a843efa178a8f7c98c63a649d8a2ab2f786
Barak Yaari <Barakyaari@gmail.com>
2016-11-10 06:55:15 -0800

    Finished Booleans

---
+++ compiler.scm
@@ -0,0 +1,33 @@
+(load "pc.scm")
+
+;; --------------------------------
+;;           Boolean:
+;; --------------------------------
+(define <Boolean>
+  (new 
+	;; True:
+  	(*parser (char #\#))
+       (*parser (char #\t))
+       (*parser (char #\T))
+       (*disj 2)
+
+       (*caten 2)
+       (*pack-with
+       	(lambda (a b)
+	  (list->string `(#\# #\t))))
+	;; False:
+  	(*parser (char #\#))
+       (*parser (char #\f))
+       (*parser (char #\F))
+       (*disj 2)
+
+       (*caten 2)
+       (*pack-with
+       	(lambda (a b)
+	  (list->string `(#\# #\f))))
+       (*disj 2)
+
+done))
+
+
+(test-string <Boolean> "#f")
